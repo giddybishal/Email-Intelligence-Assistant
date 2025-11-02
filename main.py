6e8d6a75a135f602be338470282926b8e261f098
Bishal <nbishal7@gmail.com>
2025-11-02 21:45:02 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+import pickle, os
+import base64
+from bs4 import BeautifulSoup
